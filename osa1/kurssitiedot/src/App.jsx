f373a4c30d1ec9575a8faac47edb1e49b0eb5e29
JoelTurtiainen <joel.turtiainen@gmail.com>
2024-11-22 12:30:36 +0200

    parts 1.4 & 1.5 done

--- osa1/kurssitiedot/src/App.jsx
+++ osa1/kurssitiedot/src/App.jsx
@@ -1,25 +1,29 @@
 const App = () => {
-  const course = 'Half Stack application development'
-  const part1 = {
-    name: 'Fundamentals of React',
-    exercises: 10
-  }
-  const part2 = {
-    name: 'Using props to pass data',
-    exercises: 7
-  }
-  const part3 = {
-    name: 'State of a component',
-    exercises: 14
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
   }
 
-   return (
+  return (
     <div>
-      <Header course={course} />
-      <Content parts={[part1, part2, part3]} />
-      <Total parts={[part1, part2, part3]} />
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
     </div>
-  );
+  )
 };
 
 const Header = (props) => {
@@ -38,7 +42,7 @@ const Content = ({parts}) => {
 
 const Part = ({part, exercise}) => {
   return (
-      <p>{part} {exercise}</p>
+    <p>{part} {exercise}</p>
   )
 }
 
