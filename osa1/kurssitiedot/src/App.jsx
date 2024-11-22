0257cba763fb9fedb1614f1d36ecc788119771d3
JoelTurtiainen <joel.turtiainen@gmail.com>
2024-11-22 09:20:49 +0200

    kurssitiedot done

--- osa1/kurssitiedot/src/App.jsx
+++ osa1/kurssitiedot/src/App.jsx
@@ -6,27 +6,30 @@ const App = () => {
   const exercises2 = 7;
   const part3 = "State of a component";
   const exercises3 = 14;
+  const allExcercises = [exercises1, exercises2, exercises3];
 
   return (
     <div>
-      <Header course={course}/>
-      <Content part={part1} excercises={exercises1}/>
-      <p>
-        {part2} {exercises2}
-      </p>
-      <p>
-        {part3} {exercises3}
-      </p>
-      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+      <Header course={course} />
+      <Content part={part1} excercises={exercises1} />
+      <Content part={part2} excercises={exercises2} />
+      <Content part={part3} excercises={exercises3} />
+      <Total numbers={allExcercises} />
     </div>
   );
 };
 
 const Header = (props) => {
-  return (
-    <h1>{props.course}</h1>
-  );
+  return <h1>{props.course}</h1>;
+};
+
+const Content = (props) => {
+  return <p>{props.part} {props.excercises}</p>;
+};
+
+const Total = (props) => {
+  const sum = props.numbers.reduce((acc, curr) => acc + curr, 0);
+  return <p>Number of exercises {sum}</p>;
 };
 
 export default App;
-
