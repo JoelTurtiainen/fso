6f44a77d94e7f8c292eddd0f1217aba947918ce3
JoelTurtiainen <joel.turtiainen@gmail.com>
2024-12-05 10:05:59 +0200

    3.1 - 3.8

---
+++ osa3/puhelinluettelo_backend/rest/addPerson.sh
@@ -0,0 +1,4 @@
+curl -s -i -X POST \
+    'http://localhost:3001/api/persons' \
+    -H 'Content-Type: application/json' \
+    -d '{"name": "Kari Kiuas", "number": "045-12345678"}' \
---
+++ osa3/puhelinluettelo_backend/rest/removePerson.sh
@@ -0,0 +1,3 @@
+curl -s -i -X DELETE \
+    'http://localhost:3001/api/persons/5' \
+
