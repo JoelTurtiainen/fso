7bb71da64ccaee17b8fdce3bbb5d4e7466b7554a
JoelTurtiainen <joel.turtiainen@gmail.com>
2025-03-25 14:34:12 +0200

    8.11 Edit Authors birthdate
    + Edit authors birthdate
    + Error handling (wip)

--- osa8/library-frontend/src/components/Authors.jsx
+++ osa8/library-frontend/src/components/Authors.jsx
@@ -1,22 +1,45 @@
-import { useQuery } from '@apollo/client';
-import { ALL_AUTHORS } from '../queries';
+import { useMutation, useQuery } from '@apollo/client';
+import { ALL_AUTHORS, ALL_BOOKS, EDIT_AUTHOR } from '../queries';
+import { useEffect, useState } from 'react';
 
 const Authors = (props) => {
+  const [name, setName] = useState('');
+  const [born, setBorn] = useState('');
   const result = useQuery(ALL_AUTHORS);
+
+  const [editAuthor, editResult] = useMutation(EDIT_AUTHOR, {
+    refetchQueries: [{ query: ALL_BOOKS }, { query: ALL_AUTHORS }],
+    onError: (error) => {
+      const messages = error.graphQLErrors.map((e) => e.message).join('\n');
+      console.log(messages);
+    },
+  });
+
+  useEffect(() => {
+    if (editResult.data && editResult.data.editAuthor === null) {
+      props.setError('person not found');
+    }
+  }, [editResult.data]);
 
   if (!props.show) {
     return null;
   }
+
   if (result.loading) {
     return 'loading...';
   }
 
   const authors = result.data.allAuthors;
 
+  const submit = async (event) => {
+    event.preventDefault();
+
+    editAuthor({ variables: { name, setBornTo: Number(born) } });
+    console.log(event);
+  };
   return (
     <div>
       <h2>authors</h2>
-
       <table>
         <tbody>
           <tr>
@@ -33,6 +56,18 @@ const Authors = (props) => {
           ))}
         </tbody>
       </table>
+      <form onSubmit={submit}>
+        <h2>Set Birthyear</h2>
+        <div>
+          {'name '}
+          <input value={name} onChange={({ target }) => setName(target.value)} />
+        </div>
+        <div>
+          {'born '}
+          <input value={born} onChange={({ target }) => setBorn(target.value)} />
+        </div>
+        <button type="submit">update author</button>
+      </form>
     </div>
   );
 };
--- osa8/library-frontend/src/queries.js
+++ osa8/library-frontend/src/queries.js
@@ -22,3 +22,23 @@ export const ALL_BOOKS = gql`
     }
   }
 `;
+
+export const CREATE_BOOK = gql`
+  mutation AddBook($title: String!, $author: String!, $published: Int!, $genres: [String!]!) {
+    addBook(title: $title, author: $author, published: $published, genres: $genres) {
+      title
+      author
+      published
+      genres
+    }
+  }
+`;
+
+export const EDIT_AUTHOR = gql`
+  mutation EditAuthor($name: String!, $setBornTo: Int!) {
+    editAuthor(name: $name, setBornTo: $setBornTo) {
+      born
+      name
+    }
+  }
+`;
