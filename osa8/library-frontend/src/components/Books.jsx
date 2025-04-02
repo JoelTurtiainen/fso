fcb60ca8fde4225124bed3d3c9440d1cd228d8da
JoelTurtiainen <joel.turtiainen@gmail.com>
2025-04-02 11:19:19 +0300

    8.19 filter books by genres

--- osa8/library-frontend/src/components/Books.jsx
+++ osa8/library-frontend/src/components/Books.jsx
@@ -1,14 +1,24 @@
 import { useQuery } from '@apollo/client';
 import { ALL_BOOKS } from '../queries';
+import { useEffect, useState } from 'react';
+import styles from '../style.module.css';
 
 const Books = (props) => {
+  const [genreFilter, setGenreFilter] = useState('all');
+  const [genres, setGenres] = useState(['all']);
   const result = useQuery(ALL_BOOKS);
+
+  useEffect(() => {
+    if (result.loading) return;
+    setGenres(['all', ...new Set(books.flatMap((book) => book.genres))]);
+  }, [result]);
 
   if (result.loading) {
     return 'loading...';
   }
 
   const books = result.data.allBooks;
+  const booksToShow = genreFilter === 'all' ? books : books.filter((book) => book.genres.includes(genreFilter));
 
   if (!props.show) {
     return null;
@@ -17,7 +27,20 @@ const Books = (props) => {
   return (
     <div>
       <h2>books</h2>
-
+      <div>
+        <h3>filter</h3>
+        {genres.map((genre) => (
+          <button
+            key={genre}
+            className={genreFilter === genre ? styles['button-black'] : styles['button-gray']}
+            type="button"
+            value={genre}
+            onClick={() => setGenreFilter(genre)}
+          >
+            {genre}
+          </button>
+        ))}
+      </div>
       <table>
         <tbody>
           <tr>
@@ -25,7 +48,7 @@ const Books = (props) => {
             <th>author</th>
             <th>published</th>
           </tr>
-          {books.map((a) => (
+          {booksToShow.map((a) => (
             <tr key={a.title}>
               <td>{a.title}</td>
               <td>{a.author.name}</td>
