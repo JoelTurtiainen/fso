0ef4c70b6116200263377f4f1fca29869808fb51
JoelTurtiainen <joel.turtiainen@gmail.com>
2025-04-30 09:56:29 +0300

    9.26.2 zod
    infer types from zod

--- osa9/patientor-backend/src/types.ts
+++ osa9/patientor-backend/src/types.ts
@@ -1,41 +1,17 @@
 import { z } from 'zod';
-import { newPatientSchema } from './util';
-
-interface BaseEntry {
-  id: string;
-  description: string;
-  date: string;
-  specialist: string;
-  diagnosisCodes?: Array<Diagnosis['code']>;
-}
+import {
+  diagnosisSchema,
+  healthCheckSchema,
+  hospitalSchema,
+  newPatientSchema,
+  occupationalHealthcareSchema,
+} from './util';
 
 export enum HealthCheckRating {
   'Healthy' = 0,
   'LowRisk' = 1,
   'HighRisk' = 2,
   'CriticalRisk' = 3,
-}
-
-interface HospitalEntry extends BaseEntry {
-  type: 'Hospital';
-  discharge: {
-    date: string;
-    criteria: string;
-  };
-}
-
-interface HealthCheckEntry extends BaseEntry {
-  type: 'HealthCheck';
-  healthCheckRating: HealthCheckRating;
-}
-
-interface OccupationalHealthcareEntry extends BaseEntry {
-  type: 'OccupationalHealthcare';
-  employerName: string;
-  sickLeave?: {
-    startDate: string;
-    endDate: string;
-  };
 }
 
 export type Entry = HospitalEntry | OccupationalHealthcareEntry | HealthCheckEntry;
@@ -46,24 +22,14 @@ export enum Gender {
   Other = 'other',
 }
 
-export interface Diagnosis {
-  code: string;
-  name: string;
-  latin?: string;
-}
-
-// export interface Patient {
-//   id: string;
-//   name: string;
-//   dateOfBirth: string;
-//   ssn: string;
-//   gender: Gender;
-//   occupation: string;
-// }
-
 export type NonSensitivePatientData = Omit<Patient, 'ssn' | 'entries'>;
 export type NewPatientEntry = z.infer<typeof newPatientSchema>;
 export interface Patient extends NewPatientEntry {
   id: string;
   entries: Entry[];
 }
+
+export type HospitalEntry = z.infer<typeof hospitalSchema>;
+export type HealthCheckEntry = z.infer<typeof healthCheckSchema>;
+export type OccupationalHealthcareEntry = z.infer<typeof occupationalHealthcareSchema>;
+export type Diagnosis = z.infer<typeof diagnosisSchema>;
--- osa9/patientor-backend/src/util.ts
+++ osa9/patientor-backend/src/util.ts
@@ -1,5 +1,18 @@
 import { z } from 'zod';
-import { Gender, NewPatientEntry } from './types';
+import {
+  Diagnosis,
+  Gender,
+  HealthCheckEntry,
+  HospitalEntry,
+  NewPatientEntry,
+  OccupationalHealthcareEntry,
+} from './types';
+
+export const diagnosisSchema = z.object({
+  code: z.string(),
+  name: z.string(),
+  latin: z.string().optional(),
+});
 
 export const newPatientSchema = z.object({
   name: z.string(),
@@ -9,8 +22,48 @@ export const newPatientSchema = z.object({
   occupation: z.string(),
 });
 
+const newEntrySchema = z.object({
+  id: z.string(),
+  description: z.string(),
+  date: z.string(),
+  specialist: z.string(),
+  diagnosisCodes: z.array(z.string()).optional(),
+});
+
+export const hospitalSchema = newEntrySchema.extend({
+  type: z.literal('Hospital'),
+  discharge: z.object({ date: z.string().date(), criteria: z.string() }),
+});
+
+export const healthCheckSchema = newEntrySchema.extend({
+  type: z.literal('HealthCheck'),
+  healthCheckRating: z.number(),
+});
+
+export const occupationalHealthcareSchema = newEntrySchema.extend({
+  type: z.literal('OccupationalHealthcare'),
+  employerName: z.string(),
+  sickLeave: z.object({ startDate: z.string().date(), endDate: z.string().date() }).optional(),
+});
+
 const toNewPatientEntry = (object: unknown): NewPatientEntry => {
   return newPatientSchema.parse(object);
+};
+
+export const toNewDiagnosisEntry = (object: unknown): Diagnosis => {
+  return diagnosisSchema.parse(object);
+};
+
+export const toNewHospitalEntry = (object: unknown): HospitalEntry => {
+  return hospitalSchema.parse(object);
+};
+
+export const toNewHealthCheckEntry = (object: unknown): HealthCheckEntry => {
+  return healthCheckSchema.parse(object);
+};
+
+export const toNewOccupationalHealthcareEntry = (object: unknown): OccupationalHealthcareEntry => {
+  return occupationalHealthcareSchema.parse(object);
 };
 
 export default toNewPatientEntry;
