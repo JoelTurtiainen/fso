adf2068bf0a366d6003eba60ad1b98abc16360e2
JoelTurtiainen <joel.turtiainen@gmail.com>
2025-05-15 09:43:54 +0300

    Exercise 10.7: scrollable app bar
    Wrap the tabs in the AppBar component's tabs with a ScrollView component

--- osa10/rate-repository-app/src/components/AppBar.jsx
+++ osa10/rate-repository-app/src/components/AppBar.jsx
@@ -3,30 +3,34 @@ import { Link } from 'react-router-native';
 import Constants from 'expo-constants';
 import Text from './Text';
 import theme from '../theme';
+import { ScrollView } from 'react-native';
 
 const styles = StyleSheet.create({
   container: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
     paddingTop: Constants.statusBarHeight,
     backgroundColor: theme.colors.textPrimary,
-    padding: 12,
+    padding: 5,
+  },
+  link: {
+    padding: 10,
   },
 });
 
 const AppBar = () => {
   return (
     <View style={styles.container}>
-      <Link to="/">
-        <Text fontWeight="bold" color="bgPrimary">
-          Repositories
-        </Text>
-      </Link>
-      <Link to="/signin">
-        <Text fontWeight="bold" color="bgPrimary">
-          Sign in
-        </Text>
-      </Link>
+      <ScrollView horizontal>
+        <Link to="/" style={styles.link}>
+          <Text fontWeight="bold" color="bgPrimary">
+            Repositories
+          </Text>
+        </Link>
+        <Link to="/signin" style={styles.link}>
+          <Text fontWeight="bold" color="bgPrimary">
+            Sign in
+          </Text>
+        </Link>
+      </ScrollView>
     </View>
   );
 };
